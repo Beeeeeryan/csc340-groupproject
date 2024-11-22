99fa03c94c8dfe69e595fc5972cfd1a306fe91fd
ThatCodebyTony <twu1415@gmail.com>
2024-11-22 12:10:28 -0800

    Initial setup

---
+++ src/Character/Enemy.cpp
@@ -0,0 +1,15 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <string>
+
+class Character {
+    protected:
+        std::string name; // Use std::string explicitly
+        int health;
+        int attackPower;
+
+
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Character.h"
+using namespace std;
+
+int main() {
+    cout << "Testing Character Class!" << endl;
+    return 0;
+}
